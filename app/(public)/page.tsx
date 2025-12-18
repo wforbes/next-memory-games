cb7952bbcc809b900940e76242463ddb167ed2ff
Will Forbes <wforbes87@gmail.com>
2025-12-18 00:02:11 -0800

    update the homepage with a little more info

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -8,15 +8,25 @@ export default function Home() {
 				Oh yeah! I had an idea to make a memory card game with React.
 			</p>
 			<p className="mt-4">
-				So I started doing that, you can play with it here:
+				I was interested in comparing the experience of building something by hand versus using an LLM to &apos;vibe code&apos;.
+			</p>
+			<p>
+				Stuff like how long it takes, how much better or worse the code is, and the user experience of the end result.
+			</p>
+			<p className="mt-4">
+				So I created a memory card game. You can play with it here:
 			</p>
 			<p className="mt-2">
 				<a className="text-blue-600 underline font-black" href="/memory-cards">Memory Cards Game</a>
 			</p>
+
 			<h3 className="mt-8 text-lg font-medium">
-				But I forgot what I was going to build next...
+				The &apos;handmade&apos; repo branch is where I built the game myself.
 			</h3>
-			<p>
+			<h3 className="mt-4 text-lg font-medium">
+				I can&apos;t remember if I ever vibe coded it now...
+			</h3>
+			<p className="mt-4">
 				Oh well, have fun!
 			</p>
 		</main>
